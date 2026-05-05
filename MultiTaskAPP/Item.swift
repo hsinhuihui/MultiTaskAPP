80b8006e645681bc48bc6beb5df56401b6df3a98
caixinhui <hsinhui1128@gmail.com>
2026-05-05 19:50:04 +0800

    Initial Commit

---
+++ MultiTaskAPP/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  MultiTaskAPP
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
