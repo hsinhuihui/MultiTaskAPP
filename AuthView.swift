5fbc25c186afb62c1413215f21d1a7ecf927e2fb
Minnie <peih.hung@gmail.com>
2026-05-18 14:20:55 +0800

    firebase

---
+++ AuthView.swift
@@ -0,0 +1,18 @@
+//
+//  AuthView.swift
+//  MultiTaskAPP
+//
+//  Created by 訪客使用者 on 2026/5/18.
+//
+
+import SwiftUI
+
+struct AuthView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AuthView()
+}
