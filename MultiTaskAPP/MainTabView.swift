06ffb1453cda0f495f8d9dc6c93a88146528c732
minniehung <peih.hung@gmail.com>
2026-06-02 22:28:15 +0800

    介面樣式修改

---
+++ MultiTaskAPP/MainTabView.swift
@@ -0,0 +1,18 @@
+//
+//  MainTabView.swift
+//  MultiTaskAPP
+//
+//  Created by wuyangche on 2026/6/2.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainTabView()
+}
